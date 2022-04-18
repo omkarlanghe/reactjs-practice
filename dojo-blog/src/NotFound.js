261f25aab40a27a8750d386d42cf23f1c8d23bbe
Omkar Langhe <langhe.omkar96@gmail.com>
2022-04-18 13:45:47 +0530

    Add files via upload

---
+++ dojo-blog/src/NotFound.js
@@ -0,0 +1,11 @@
+const NotFound = () => {
+    return (
+        <div className="not-found">
+            <h2>Sorry</h2>
+            <p>That page cannot be found</p>
+            <a href="/">Back to the homepage...</a>
+        </div>
+    );
+}
+ 
+export default NotFound;
